59ce3e55342fc187f66052a20dd706bc3a596f5a
myeonghwan <1jmhcho@gmail.com>
2023-10-18 15:13:40 +0900

    스프링 시큐리티 OAuth2.0
    구글 로그인 진행 완료

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.cos.security1.config;
+// 1.코드받기(인증), 2.엑세스토큰(권한), 3.사용자프로필 정보를 가져옴
+// 4-1.그 정보를 토대로 회원가입을 자동으로 진행시킴
+// 4-2.추가적인 정보 입력
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,12 +18,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
 public class SecurityConfig{
 
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
+
     // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록
+    /*
     @Bean
     public BCryptPasswordEncoder encodePwd() {
         return new BCryptPasswordEncoder();
     }
-
+        순환참조가 발생하여 CustomBCryptPasswordEncoder 컨테이너에 등록
+    */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -31,8 +41,12 @@ public class SecurityConfig{
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행함
-                .defaultSuccessUrl("/");
-
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함. Tip. 코드X (엑세스토큰+사용자프로필정보 O)
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
         return http.build();
     }
 
--- src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
+++ src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
@@ -7,18 +7,31 @@ package com.cos.security1.config.auth;
 // User 오브젝트 타입 => UserDetails 타입 객체
 
 import com.cos.security1.model.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 // Security Session => Authentication => UserDetails(PrincipalDetails)
-public class PrincipalDetails implements UserDetails{
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user; //콤포지션
+    private Map<String, Object> attributes;
+    
+    // 일반 로그인
     public PrincipalDetails(User user) {
         this.user = user;
+    }
+
+    // OAuth 로그인
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
     }
 
     // 해당 User의 권한을 리턴하는 곳
@@ -65,5 +78,14 @@ public class PrincipalDetails implements UserDetails{
         // 현재시간 - 로그인 시간 => 1년을 초과하면 return false
         
         return true;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+    @Override
+    public String getName() {
+        return null;
     }
 }
